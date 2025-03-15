35a8ef099e202825f5f5ca76f127c6503ab73bcf
Stormc0de <158101702+Stormc0de@users.noreply.github.com>
2025-03-15 03:10:24 -0400

    Added HTML to about and projects

--- main.js
+++
@@ -1,8 +0,0 @@
-// Select the toggle button and navigation menu
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-// Add click event to toggle menu visibility
-menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-});
